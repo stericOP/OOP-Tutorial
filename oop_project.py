7e1b91e586589933e10cdd109af8ba219cc7c9cb
stericOP <hariomshipalkar@gmail.com>
2024-12-29 11:30:20 +0530

    Project- Signup-Signin Completed

--- oop_project.py
+++ oop_project.py
@@ -14,14 +14,38 @@ class Chatbook:
                            5- Press any key to signup""")
 
     if user_input == '1':
-      pass
+      self.signup()
     elif user_input == '2':
-      pass
+      self.signin()
     elif user_input == '3':
       pass
     elif user_input == '4':
       pass
     else:
       exit()
+
+  def signup(self):
+    email = input("Enter your email here -> ")
+    pswd = input("Setup your password -> ")
+    self.username = email
+    self.password = pswd
+    print("You have signed up to ChatBook")
+    print("\n")
+    self.menu() 
+
+  def signin(self):
+    if self.username == '' and self.password == '':
+      print("Please Signup first by clicking 1")
+    else:
+      username = input("Enter your email/username here -> ")
+      pswd = input("enter your password here -> ")
+      if(self.username == username and self.password == pswd):
+        print("You have signed in successfully")
+        self.loggedin = True
+      else: 
+        print("Incorrect username/password")
+    print("\n")
+    self.menu()  
+
 
 chat = Chatbook()
